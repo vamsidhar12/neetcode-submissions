d6e76ea56e74016338067db08733737bf71b5883
Vamsidhar Reddy Poothi <vamsidhar.reddy1207@gmail.com>
2026-04-09 16:52:22 -0500

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,24 @@
+class MedianFinder {
+
+    PriorityQueue<Integer> max;
+    PriorityQueue<Integer> min;
+
+    public MedianFinder() {
+        this.min = new PriorityQueue<>((a,b) -> a - b);
+        this.max = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        max.offer(num);
+        min.offer(max.poll());
+        if(max.size() < min.size())
+            max.add(min.poll());
+    }
+    
+    public double findMedian() {
+        if(max.size() == min.size())
+            return (max.peek() + min.peek()) / 2.0;
+        else
+            return max.peek() * 1.0;
+    }
+}
