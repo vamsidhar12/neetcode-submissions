215061d6eb890a4002165b6527a82070abfe03a5
Vamsidhar Reddy Poothi <vamsidhar.reddy1207@gmail.com>
2026-04-10 15:32:41 -0500

    Add: find-the-town-judge - submission-4

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegree = new int[n + 1];
+        int[] outdegree = new int[n + 1];
+        for(int[] t : trust) {
+            indegree[t[1]]++;
+            outdegree[t[0]]++;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(outdegree[i] == 0 && indegree[i] == n - 1)
+                return i; 
+        }
+        return -1;
+    }
+}
